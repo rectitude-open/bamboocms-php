4236875e5955c0708679127a1c8e693c5acb9cea
aspirantzhang <admin@aspirantzhang.com>
2024-06-28 17:10:34 +0800

    feat: add global getRoute function for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,5 +8,17 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected array $routes = [];
+
+    protected string $tableName = '';
+
+    protected function getRoute($name, $params = [])
+    {
+        $routeName = $this->routes[$name] ?? null;
+        if (! $routeName) {
+            throw new \Exception("Route name not found: $name");
+        }
+
+        return route($routeName, $params);
+    }
 }

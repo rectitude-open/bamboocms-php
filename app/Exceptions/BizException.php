99186c904a2743bae0dd72255fcbf0f3e5cc07f0
aspirantzhang <admin@aspirantzhang.com>
2024-07-02 14:37:49 +0800

    feat: add BizException and Builder

--- app/Exceptions/BizException.php
+++ app/Exceptions/BizException.php
@@ -5,5 +5,54 @@ declare(strict_types=1);
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
-class BizException extends Exception {}
+class BizException extends Exception
+{
+    public function __construct(
+        string $message = '',
+        protected string $logMessage = '',
+        protected array $context = [],
+        int $code = 400,
+        ?Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function setFile(string $file): void
+    {
+        $this->file = $file;
+    }
+
+    public function setLine(int $line): void
+    {
+        $this->line = $line;
+    }
+
+    public function report(): void
+    {
+        $previousTrace = $this->getPrevious();
+        $previousTrace = $this->getPrevious() ? explode("\n", $this->getPrevious()->getTraceAsString()) : [];
+        $previousTraceSlice = array_slice($previousTrace, 0, 5);
+        $previousTraceString = implode("\n", $previousTraceSlice);
+
+        $logEntry = [
+            'message' => $this->logMessage,
+            'context' => $this->context,
+            'file' => $this->getFile(),
+            'line' => $this->getLine(),
+            'previous' => $previousTraceString,
+        ];
+        Log::info('BizException', $logEntry);
+    }
+
+    public function render(Request $request): JsonResponse
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $this->message,
+        ], $this->getCode());
+    }
+}
---
+++ app/Http/Support/BizExceptionBuilder.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Support;
+
+use App\Exceptions\BizException;
+use Exception;
+
+class BizExceptionBuilder
+{
+    protected string $message;
+
+    protected string $logMessage = '';
+
+    protected array $context = [];
+
+    protected int $code = 400;
+
+    protected ?Exception $previous = null;
+
+    public static function make(string $message): self
+    {
+        $instance = new self();
+        $instance->message = $message;
+
+        return $instance;
+    }
+
+    public function logMessage(string $logMessage): self
+    {
+        $this->logMessage = $logMessage;
+
+        return $this;
+    }
+
+    public function context(array $context): self
+    {
+        $this->context = $context;
+
+        return $this;
+    }
+
+    public function code(int $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function previous(?Exception $previous): self
+    {
+        $this->previous = $previous;
+
+        return $this;
+    }
+
+    public function throw(): void
+    {
+        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 1)[0];
+
+        $exception = new BizException(
+            $this->message,
+            $this->logMessage,
+            $this->context,
+            $this->code,
+            $this->previous
+        );
+        $exception->setFile($trace['file']);
+        $exception->setLine($trace['line']);
+        throw $exception;
+    }
+}

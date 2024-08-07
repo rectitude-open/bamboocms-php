4c0b29e82a16813fc84594110b740495da6b64ef
aspirantzhang <admin@aspirantzhang.com>
2024-08-07 10:34:22 +0800

    refactor: BaseFormRequest to support json format sorting

--- app/Http/Requests/BaseFormRequest.php
+++ app/Http/Requests/BaseFormRequest.php
@@ -11,43 +11,42 @@ class BaseFormRequest extends FormRequest
     protected function prepareForValidation()
     {
         parent::prepareForValidation();
+
+        $this->castInputValues();
+    }
+
+    protected function castInputValues()
+    {
+        $casts = [
+            'id' => 'int',
+            'per_page' => 'int',
+            'current_page' => 'int',
+            'pagination' => 'boolean',
+            'sorting' => 'sorting',
+        ];
+
+        foreach ($casts as $field => $type) {
+            if ($this->has($field)) {
+                $this->merge([$field => $this->castValue($this->input($field), $type)]);
+            }
+        }
     }
 
     public function baseIndexRules(): array
     {
         return [
             'id' => ['integer', 'gte:1'],
-            'sort' => ['string', 'min:2', 'max:255'],
-            'order' => ['string', 'regex:/^(desc|asc)$/i'],
             'per_page' => ['integer', 'gte:1', 'lte:100'],
             'current_page' => ['integer', 'gte:1'],
             'pagination' => ['in:true,false'],
-            'with_trashed' => ['in:true,false'],
             'start_date' => ['date_format:Y-m-d H:i:s,Y-m-d', 'before_or_equal:end_date'],
             'end_date' => ['date_format:Y-m-d H:i:s,Y-m-d', 'after_or_equal:start_date'],
             'start_time' => ['date_format:Y-m-d H:i:s,Y-m-d', 'before_or_equal:end_time'],
             'end_time' => ['date_format:Y-m-d H:i:s,Y-m-d', 'after_or_equal:start_time'],
+            'sorting' => ['nullable', 'array'],
+            'sorting.*.id' => ['required', 'string'],
+            'sorting.*.desc' => ['required', 'boolean'],
         ];
-    }
-
-    public function validated($key = null, $default = null)
-    {
-        $validatedData = parent::validated($key, $default);
-
-        $casts = [
-            'id' => 'int',
-            'per_page' => 'int',
-            'current_page' => 'int',
-            'pagination' => 'boolean',
-        ];
-
-        foreach ($casts as $field => $type) {
-            if (isset($validatedData[$field])) {
-                $validatedData[$field] = $this->castValue($validatedData[$field], $type);
-            }
-        }
-
-        return $validatedData;
     }
 
     protected function castValue($value, $type)
@@ -57,6 +56,8 @@ class BaseFormRequest extends FormRequest
                 return (int) $value;
             case 'boolean':
                 return filter_var($value, FILTER_VALIDATE_BOOLEAN);
+            case 'sorting':
+                return json_decode($value, true);
             default:
                 return $value;
         }

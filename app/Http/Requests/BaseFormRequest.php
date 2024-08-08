c00926557e6c4068ca2be709d17b6b5a8bc8964d
aspirantzhang <admin@aspirantzhang.com>
2024-08-08 11:43:30 +0800

    feat: Add support for filtering with dynamic values in BaseFormRequest

--- app/Http/Requests/BaseFormRequest.php
+++ app/Http/Requests/BaseFormRequest.php
@@ -4,17 +4,29 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
+use App\Rules\ValidFilterValue;
 use App\Rules\ValidSorting;
 use Illuminate\Foundation\Http\FormRequest;
 
 class BaseFormRequest extends FormRequest
 {
+    protected function prepareForValidation()
+    {
+        if ($this->has('filters')) {
+            $filters = json_decode($this->input('filters'), true);
+
+            if (json_last_error() === JSON_ERROR_NONE) {
+                $this->merge(['filters' => $filters]);
+            }
+        }
+    }
+
     protected array $casts = [
         'id' => 'int',
         'per_page' => 'int',
         'current_page' => 'int',
         'pagination' => 'boolean',
-        'sorting' => 'sorting',
+        'sorting' => 'json',
     ];
 
     public function validated($key = null, $default = null)
@@ -47,6 +59,9 @@ class BaseFormRequest extends FormRequest
             'start_time' => ['date_format:Y-m-d H:i:s,Y-m-d', 'before_or_equal:end_time'],
             'end_time' => ['date_format:Y-m-d H:i:s,Y-m-d', 'after_or_equal:start_time'],
             'sorting' => ['nullable', 'string', new ValidSorting],
+            'filters' => ['array'],
+            'filters.*.id' => ['required', 'string'],
+            'filters.*.value' => ['required', (new ValidFilterValue($this->filterRules()))],
         ];
     }
 
@@ -57,7 +72,7 @@ class BaseFormRequest extends FormRequest
                 return (int) $value;
             case 'boolean':
                 return filter_var($value, FILTER_VALIDATE_BOOLEAN);
-            case 'sorting':
+            case 'json':
                 return json_decode($value, true);
             default:
                 return $value;
---
+++ app/Rules/ValidFilterValue.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\DataAwareRule;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Validator;
+
+class ValidFilterValue implements DataAwareRule, ValidationRule
+{
+    protected $data = [];
+
+    public function __construct(private array $filterRules = []) {}
+
+    public function setData(array $data): static
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $parts = explode('.', $attribute);
+        $filterIndex = $parts[1];
+        $filterId = $this->data['filters'][$filterIndex]['id'] ?? null;
+
+        if ($filterId && isset($this->filterRules[$filterId])) {
+            $validator = Validator::make(
+                ['value' => $value],
+                ['value' => $this->filterRules[$filterId]]
+            );
+
+            if ($validator->fails()) {
+                foreach ($validator->errors()->all() as $message) {
+                    $fail($message);
+                }
+            }
+        } else {
+            $fail("Invalid filter id: $filterId");
+        }
+    }
+}

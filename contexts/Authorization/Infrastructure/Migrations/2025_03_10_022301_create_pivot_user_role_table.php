5a3e7cf27877d97be35c88c1f253ce9bcda5eeab
aspirantzhang <admin@aspirantzhang.com>
2025-03-31 14:17:12 +0800

    feat: update migration down methods to drop pivot tables for articles and user roles

--- contexts/Authorization/Infrastructure/Migrations/2025_03_10_022301_create_pivot_user_role_table.php
+++ contexts/Authorization/Infrastructure/Migrations/2025_03_10_022301_create_pivot_user_role_table.php
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('pivot_user_role');
     }
 };

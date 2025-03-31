63cc36309274c2b23b84fed6344c948139f7b5d9
aspirantzhang <admin@aspirantzhang.com>
2025-03-31 14:38:21 +0800

    feat: implement database seeder for admin user and role creation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use Contexts\Authorization\Infrastructure\Records\RoleRecord;
+use Contexts\Authorization\Infrastructure\Records\UserRecord;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,5 +13,20 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void {}
+    public function run(): void
+    {
+        $adminUser = UserRecord::factory()->create([
+            'email' => 'admin@test.com',
+            'password' => password_hash('test', PASSWORD_ARGON2ID),
+            'display_name' => 'Admin',
+            'status' => 1,
+        ]);
+
+        $adminRole = RoleRecord::factory()->create([
+            'label' => 'admin',
+            'status' => 1,
+        ]);
+
+        $adminUser->roles()->attach($adminRole->id);
+    }
 }
